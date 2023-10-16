0090dbee9d659c49d94a47fbc30b0e59324cc66d
Himanshu Shakya <shakyahimanshu2002@gmail.com>
2023-10-16 12:47:56 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,43 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import ThreadCard from "@/components/cards/ThreadCard";
+// import Pagination from "@/components/shared/Pagination";
+
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+export default async function Home() {
+  const result= await fetchPosts(1,30);
+  const user = await currentUser();
+
+  console.log(result);
+
+  return (
+    <>
+    <h1 className="head-text text-left">Home</h1>
+
+    <section className='mt-9 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id || ""}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+    </>
+  )
+}
---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,60 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import Image from "next/image";
+import { profileTabs } from "@/constants";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+
+async function Page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+
+      {/* <Searchbar routeType="search" /> */}
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No Result</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+
+      {/* <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      /> */}
+    </section>
+  );
+}
+
+export default Page;
